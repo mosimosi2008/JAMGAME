c2b22ff9c6b0b6d643e2f0f6a1ba963066bf3709
mosi <mosi66216@gmail.com>
2025-02-16 15:52:54 +0000

    Add files via upload
    -Chidum has implemented the first disappearing floor trap

--- a-nice-walk/level.gd
+++ a-nice-walk/level.gd
@@ -4,11 +4,13 @@ func _ready():
 	trap_layer.collision_enabled = true
 @onready var trap_layer = $TrapLayer
 @onready var player = $Player
-
+@onready var anim = $AnimationPlayer
 
 
 
 func trap_floor_entered(body: Node2D) -> void:
-	if body == player:
-		trap_layer.collision_enabled = false
-		trap_layer.visible = false
+	if body == player and trap_layer.collision_enabled == true:
+		anim.play("trap_floor_disappear")
+		
+		
+	
