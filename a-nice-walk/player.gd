316660b8084a2465cc158d2bca65e6810266e0ff
mosi <mosi66216@gmail.com>
2025-02-17 20:46:04 +0000

    Add files via upload
    -added camera

--- a-nice-walk/player.gd
+++ a-nice-walk/player.gd
@@ -19,8 +19,7 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = move_toward(velocity.x,direction * MAX_SPEED,ACC) # this took an embrassing amount of time
-		print(velocity.x) 			
+		velocity.x = move_toward(velocity.x,direction * MAX_SPEED,ACC) # this took an embrassing amount of time	
 	else:
 		velocity.x = move_toward(velocity.x, 0, 10)
 
